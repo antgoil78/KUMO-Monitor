7be9f9584b11764a8d797a167793cf766802a4f4
antgoil78 <andreas@openresult.se>
2026-06-17 13:36:31 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-export FLASK_APP=backend/app.py
+export PYTHONPATH=/app/backend:/app:${PYTHONPATH}
+
+export FLASK_APP=backend.app
 export FLASK_ENV=production
+export PYTHONUNBUFFERED=1
 
 echo "Starting Flask backend server..."
 python -m flask run --host=0.0.0.0 --port=5000 &
